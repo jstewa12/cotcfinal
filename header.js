1c1632f99de1d22b7c9c3825c509fabc969bb412
JJ Stewart <jstewa12@tufts.edu>
2020-04-21 23:33:26 -0400

    whole project

---
+++ header.js
@@ -0,0 +1,6 @@
+document.write("<header><div class='nav'>" +
+    "<a href='index.html'>Main Page</a>" +
+    "<a href='planetnav.html'>Rogue Planet Profiles</a>" +
+    "<a href='planetinfo.html'>Info about Rogue Planets</a>" +
+    "<a href='rogueearth.html'>Earth as a Rogue Planet</a></div></header>"
+);
